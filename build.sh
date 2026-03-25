3b05c0b0d0b0c21f36a691734c81a2267533459d
rehaan <97499666+DatDevSteve@users.noreply.github.com>
2026-03-25 23:21:53 +0530

    fixed flutter version

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/flutter/flutter.git --depth 1
+git clone https://github.com/flutter/flutter.git --branch stable --depth 1
+
 export PATH="$PATH:`pwd`/flutter/bin"
 
+flutter precache
 flutter config --enable-web
 flutter pub get
 flutter build web --release
