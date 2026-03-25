4583b3af9e2762fa8f6a91351ab5d8e16562e405
rehaan <97499666+DatDevSteve@users.noreply.github.com>
2026-03-25 23:02:37 +0530

    Replace Placeholder with Coming Soon message in ComingSoon widget

--- lib/coming_soon.dart
+++ lib/coming_soon.dart
@@ -5,6 +5,11 @@ class ComingSoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Center(child: Text("COMING SOON!"))],
+      ),
+    );
   }
 }
